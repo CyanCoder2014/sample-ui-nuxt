c75a1714c8847736c3b6b0bf94f7d47edbdbd8bb
farid-cyan <info@cyancoder.co>
2021-09-25 15:23:08 +0330

    add chart plugin and dev pages

---
+++ components/ChartBar.vue
@@ -0,0 +1,101 @@
+<template>
+
+
+
+  <div>
+
+
+
+
+    <line-chart v-if="datacollection" :chart-data="datacollection"></line-chart>
+
+
+  </div>
+
+
+
+</template>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+<script>
+const gradients = [
+  ['#222'],
+  ['#42b3f4'],
+  ['red', 'orange', 'yellow'],
+  ['purple', 'violet'],
+  ['#00c6ff', '#F0F', '#FF0'],
+  ['#f72047', '#ffd200', '#1feaea'],
+]
+
+import LineChart from '../plugins/vue-chart.js'
+
+
+export default {
+
+  components: {
+    LineChart
+  },
+
+  data: () => ({
+    width: 2,
+    radius: 10,
+    padding: 8,
+    lineCap: 'round',
+    gradient: gradients[5],
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    gradientDirection: 'top',
+    gradients,
+    fill: false,
+    type: 'trend',
+    autoLineWidth: false,
+
+
+    datacollection: null
+
+
+
+  }),
+
+
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#99eeff',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#ffeeee',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
+
+
+}
+</script>
